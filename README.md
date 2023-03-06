86df43b5eb3a310e750bd898690f042cdd47b8e0
Joakim Sundman <82515307+jayhyllie@users.noreply.github.com>
2023-03-06 10:02:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Todo app
+
+![Design preview for the Todo app](./design/desktop-preview.jpg)
+
+Available for both desktop and mobile
---
+++ main.js
@@ -0,0 +1,61 @@
+const todoList = document.querySelector('.main__todo--list');
+const input = document.querySelector('.header form input');
+const itemLeft = document.querySelector('.main__todo--states .states__items-left span');
+let inputValue = input.value;
+let count = 0;
+itemLeft.innerHTML = count;
+
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        addTodo();
+        itemLeft.innerHTML = count++;
+    }
+})
+
+function addTodo() {
+    const todoText = input.value.trim();
+    if (todoText) {
+        const li = document.createElement('li');
+        li.classList.add('main__todo--list-item');
+        const checkBox = document.createElement('input')
+        checkBox.classList.add('added--todo');
+        checkBox.setAttribute('type', 'checkbox');
+        const title = document.createElement('h2');
+        title.classList.add('added--todo-title');
+        const span = document.createElement('span');
+        span.classList.add('todo__remove');
+        span.innerText = 'X';
+
+        span.addEventListener('click', () => {
+            li.remove();
+            itemLeft.innerHTML = count--;
+        });
+
+        checkBox.addEventListener('click', () => {
+            li.classList.toggle('done');
+        })
+
+        title.innerText = todoText;
+        li.appendChild(checkBox)
+        li.appendChild(title);
+        li.appendChild(span);
+        todoList.appendChild(li);
+        input.value = '';
+    }
+}
+
+function darkMode() {
+    const moon = document.querySelector('.header figure .moon');
+    const sun = document.querySelector('.header figure .sun');
+
+    moon.addEventListener('click', () => {
+        moon.classList.remove('active')
+        sun.classList.add('active')
+    })
+    sun.addEventListener('click', () => {
+        sun.classList.remove('active')
+        moon.classList.add('active')
+    })
+}
+darkMode();
