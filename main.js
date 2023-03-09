c1716d6ee25b82609acbce7a2f26a594c4ad790f
Joakim Sundman <82515307+jayhyllie@users.noreply.github.com>
2023-03-09 22:00:57 +0100

    fixed filtering

--- main.js
+++ main.js
@@ -1,58 +1,110 @@
-const todoList = document.querySelector('.main__todo--list');
-const input = document.querySelector('.header form input');
+const newTaskInput = document.querySelector('.header form input');
+const taskList = document.querySelector('.main__todo--list');
 const itemLeft = document.querySelector('.main__todo--states .states__items-left span');
+const showAllButton = document.querySelector('#all');
+const showActiveButton = document.querySelector('#active');
+const showCompletedButton = document.querySelector('#completed');
 
-let inputValue = input.value;
+let tasks = [];
 let count = 0;
 itemLeft.innerHTML = count;
 
-input.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        addTodo();
+newTaskInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        addTask();
         count++;
         itemLeft.innerHTML = count;
     }
-})
+});
 
-function addTodo() {
-    const todoText = input.value.trim();
-    if (todoText) {
-        const li = document.createElement('li');
-        li.classList.add('main__todo--list-item');
-        const checkBox = document.createElement('input')
-        checkBox.classList.add('added--todo');
-        checkBox.setAttribute('type', 'checkbox');
-        const title = document.createElement('h2');
-        title.classList.add('added--todo-title');
+showAllButton.addEventListener('click', showAllTasks);
+showActiveButton.addEventListener('click', showActiveTasks);
+showCompletedButton.addEventListener('click', showCompletedTasks);
+
+function addTask() {
+    const taskName = newTaskInput.value.trim();
+    if (taskName) {
+        const task = { name: taskName, completed: false };
+        tasks.push(task);
+        updateTaskList();
+        newTaskInput.value = '';
+    }
+}
+
+function updateTaskList() {
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const taskItem = document.createElement('li');
+        taskItem.classList.add('main__todo--list-item');
+        taskItem.setAttribute('data-done', 'no')
+        const taskCheckbox = document.createElement('input');
+        taskCheckbox.classList.add('added--todo');
+        taskCheckbox.type = 'checkbox';
+        taskCheckbox.checked = task.completed;
+        taskCheckbox.addEventListener('change', () => {
+            task.completed = taskCheckbox.checked;
+            if (task.completed) {
+                count--;
+            } else {
+                count++;
+            }
+            itemLeft.innerHTML = count;
+            updateTaskList();
+        });
+        const taskLabel = document.createElement('label');
+        taskLabel.classList.add('added--todo-title');
+        taskLabel.textContent = task.name;
         const span = document.createElement('span');
         span.classList.add('todo__remove');
         span.innerText = 'X';
-
         span.addEventListener('click', () => {
-            li.remove();
+            taskItem.remove();
             count--;
             itemLeft.innerHTML = count;
         });
-
-        checkBox.addEventListener('click', () => {
-            if (li.classList.contains('done')) {
-                count++;
-            } else {
-                count--;
-            }
-            li.classList.toggle('done');
-            itemLeft.innerHTML = count;
-        })
-
-        title.innerText = todoText;
-        li.appendChild(checkBox)
-        li.appendChild(title);
-        li.appendChild(span);
-        todoList.appendChild(li);
-        input.value = '';
-    }
+        taskItem.appendChild(taskCheckbox);
+        taskItem.appendChild(taskLabel);
+        taskItem.appendChild(span);
+        if (task.completed) {
+            taskItem.classList.add('done');
+        }
+        taskList.appendChild(taskItem);
+    });
 }
+
+function showAllTasks() {
+    showAllButton.classList.add('active');
+    showActiveButton.classList.remove('active');
+    showCompletedButton.classList.remove('active');
+    tasks.forEach((task) => {
+        taskList.childNodes[getIndex(task)].style.display = 'flex';
+    });
+}
+
+function showActiveTasks() {
+    showAllButton.classList.remove('active');
+    showActiveButton.classList.add('active');
+    showCompletedButton.classList.remove('active');
+    tasks.forEach((task) => {
+        taskList.childNodes[getIndex(task)].style.display = task.completed ? 'none' : 'flex';
+    });
+}
+
+function showCompletedTasks() {
+    showAllButton.classList.remove('active');
+    showActiveButton.classList.remove('active');
+    showCompletedButton.classList.add('active');
+    tasks.forEach((task) => {
+        taskList.childNodes[getIndex(task)].style.display = task.completed ? 'flex' : 'none';
+    });
+}
+
+function getIndex(task) {
+    return tasks.indexOf(task);
+}
+
+showAllTasks();
 
 function darkMode() {
     const moon = document.querySelector('.header figure .moon');
